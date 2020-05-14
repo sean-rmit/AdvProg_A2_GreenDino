47e66f0cbb9a10f1b491fa893788b46645db53d0
Aaron Soa <48776008+s3786067@users.noreply.github.com>
2020-05-14 23:49:07 +1000

    Load function updated

--- gameFunc/LoadSaveFile.cpp
+++ gameFunc/LoadSaveFile.cpp
@@ -1,65 +1,134 @@
 #include<fstream>
 #include<iostream>
-#include<cstring>
+
+void getData(std::string line);
+void splitData(std::string data);
 
 //Read from file
 int main() {
     //Reading file
+
     std::ifstream savedFile("saveFile.txt");
-    std::string line;
+    while(!savedFile.eof()) {
+        std::string line;
+        for (int i = 0; getline(savedFile, line) && i < 35; i++) {
+            int j;
 
-    if (!savedFile.eof()) {
-        if (line.find("factory1")) {
-            getline(savedFile, line);
-            std::cout << line << std::endl;
+            //BAG
+            if (i == 0) {
+                getData(line);
+            }
+
+            //LID
+            if (i == 1) {
+                getData(line);
+            }
+
+            //FACTORIES ----------------------------
+            //factory centre
+            if (i == 2) {
+                getData(line);
+            }
+            
+            //factory 1-5
+            for (j = 3; j <= 7; j++) {
+                if (i == j) {
+                    getData(line);
+                }
+            }
+
+            //PLAYER_1 -------------------------------
+            //name
+            if (i == 8) {
+                getData(line);
+            }
+
+            //score
+            if (i == 9) {
+                getData(line);
+            }
+
+            //patternLine1-5
+            for (j = 10; j <= 14; j++) {
+                if (i == j) {
+                    getData(line);
+                }
+            }
+            
+            //floorLine
+            if (i == 15) {
+                getData(line);
+            }
+
+            //Mosaic1-5
+            for (j = 16; j <= 20; j++) {
+                if (i == j) {
+                    getData(line);
+                }
+            }
+            
+            //PLAYER_2 --------------------------------
+            //P2name
+            if (i == 21) {
+                getData(line);
+            }
+
+            //P2score
+            if (i == 22) {
+                getData(line);
+            }
+
+            //P2patternLine1-5
+            for (j = 23; j <= 27; j++) {
+                if (i == j) {
+                    getData(line);
+                }
+            }
+
+            //P2floorLine
+            if (i == 28) {
+                getData(line);
+            }
+            
+            //P2Mosaic_0
+            for (j = 29; j <= 33; j++) {
+                if (i == j) {
+                    getData(line);
+                }
+            }
+
+            //NUMBER OF PLAYERS------------------------
+            //currentPlayers
+            if (i == 34) {
+                getData(line);
+            }
         }
-        if (line.find("factory2")) {
-            getline(savedFile, line);
-            std::cout << line << std::endl;
-        }
-        if (line.find("factory3")) {
-            getline(savedFile, line);
-            std::cout << line << std::endl;
-        }
-        if (line.find("factory4")) {
-            getline(savedFile, line);
-            std::cout << line << std::endl;
-        }
-        if (line.find("factory5")) {
-            getline(savedFile, line);
-            std::cout << line << std::endl;
-        }
-    }
-   
     
-    std::cout << std::endl;
-    std::cout << "all file info: " << std::endl;
-    while (!savedFile.eof()) {
-        std::string out;
-
-        getline(savedFile, out);
-        std::cout << out << std:: endl;
-
     }
 
-    return EXIT_SUCCESS;
 }
 
+void getData(std::string line) {
+    int ll;
+    std::string data;
+    char e = '='; 
+    size_t found = line.find(e); 
+    int el = 0;
+    if (found != std::string::npos) {
+        el = found + 1;
+    }
+    ll = line.length();
 
+    data = line.substr(el, ll-el);
+    //std::cout << data << std::endl;
 
-// readSaveFile(savefile, player, *factory1-5, *centre, lid, bag) {
-//     //read file line by line
-//     for (filename.hasnext) {
-//         String line = filename.getline()
-//         if (line contains "factory1") {
-//             String[] tokens = line.split(" ")
-//             for each (tokens[2]) {
-//                 char array = token[2];
-//                 factory1->addTile(array[i]);
+    splitData(data);
+}
 
-//             }
-
-//         }
-//     }
-//     centre lid bag
-// }
+void splitData(std::string data) {
+    int k;
+    int tll = data.length();
+    for (k = 0; k < tll; k++) {
+        std::cout << data[k] << std::endl;
+    }
+}
