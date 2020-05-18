62015b5b06c6025844cbf09c4380b94527b75995
s3786067 <s3786067@student.rmit.edu.au>
2020-05-18 23:58:53 +1000

    changed load file to seach by keyword

--- gameFunc/LoadSaveFile.cpp
+++ gameFunc/LoadSaveFile.cpp
@@ -21,7 +21,8 @@ void LoadSave::saveFile(std::string saveFile, std::string player1, std::string p
 }
 
 /* Change here */
-void LoadSave::loadFile(std::string loadFile, Factories *factories, Player *player1)
+void LoadSave::loadFile(std::string loadFile)
+//, Factories *factories, Player *player1
 {   
     // Reading file
     std::ifstream savedFile(loadFile);
@@ -52,293 +53,184 @@ void LoadSave::loadFile(std::string loadFile, Factories *factories, Player *play
 
     //while loop to read everything
     std::string line;
-    while (std::getline(savedFile, line)) {
-        int tll, k;
+    while (getline(savedFile, line)) {
         std::string data;
-        getData(line, data);
+        int k, dl;
 
-        // split data
-        tll = data.length();
-        for (k = 0; k < tll; k++)
-        {
-            std::cout << data[k] << std::endl;
-        }
+        //FACTORIES +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //Finding keyword line
+        if (line.find("BAG") != std::string::npos) {
+            //Getting all the information after the '=' sign
+            getData(line, data);
 
-        std::string gameObject = data[0]; // i'm not sure how you split the stuff but this should be the first string
-
-        if (gameObject == "factory1"){
-            // add the tiles in a loop
-            for (int i = 0; i < arraywithallthetiles.size(); i++) {
-                factories->getFactory(0)->getLine()->addTileToBack(arraywithallthetiles[i]);
+            //Splitting data into individual characters
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
             }
         }
-        else if (gameObject == "player1") {
-            // read if this wall or patternlines or brokenlines
-            // add the tiles in a loop
-            for (int i = 0; i < arraywithallthetiles.size(); i++) {
-                // get the lineIndex too
-                player1->getPlayerMosaic()->getPlayerPatternLines()->getLine(lineIndex)->addTileToBack(arraywithallthetiles[i]);
+
+        if (line.find("LID") != std::string::npos) {
+            getData(line, data);
+
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
+            }
+        }
+
+        if (line.find("FACTORY_CENTRE") != std::string::npos) {
+            getData(line, data);
+
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
+            }
+        }
+
+        for (int i = 0; i <= 4; i++) {
+            std::string factories = "FACTORY_";
+            factories += std::to_string(i);
+            if (line.find(factories) != std::string::npos) {
+                getData(line, data);
+
+                dl = data.length();
+                for (k = 0; k < dl; k++) {
+                    std::cout << data[k] << std::endl;
+                }
+            }
+        }
+
+        //PLAYER_1 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        if (line.find("PLAYER_1_NAME") != std::string::npos) {
+            getData(line, data);
+
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
+            }
+        }
+
+        if (line.find("PLAYER_1_SCORE") != std::string::npos) {
+            getData(line, data);
+
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
+            }
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            std::string playerPattern = "PLAYER_1_PATTERN_LINE";
+            playerPattern += std::to_string(i);
+            if (line.find(playerPattern) != std::string::npos) {
+                getData(line, data);
+
+                dl = data.length();
+                for (k = 0; k < dl; k++) {
+                    std::cout << data[k] << std::endl;
+                }
+            }
+        }
+
+        if (line.find("PLAYER_1_FLOOR_LINE") != std::string::npos) {
+            getData(line, data);
+
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
+            }
+        }
+
+        for (int i = 0; i <= 4; i++) {
+            std::string playerMosaic = "PLAYER_1_MOSAIC_";
+            playerMosaic += std::to_string(i);
+            if (line.find(playerMosaic) != std::string::npos) {
+                getData(line, data);
+
+                dl = data.length();
+                for (k = 0; k < dl; k++) {
+                    std::cout << data[k] << std::endl;
+                }
+            }
+        }
+
+        //PLAYER_2 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        if (line.find("PLAYER_2_NAME") != std::string::npos) {
+            getData(line, data);
+
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
+            }
+        }
+
+        if (line.find("PLAYER_2_SCORE") != std::string::npos) {
+            getData(line, data);
+
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
+            }
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            std::string playerPattern = "PLAYER_2_PATTERN_LINE";
+            playerPattern += std::to_string(i);
+            if (line.find(playerPattern) != std::string::npos) {
+                getData(line, data);
+                
+                dl = data.length();
+                for (k = 0; k < dl; k++) {
+                    std::cout << data[k] << std::endl;
+                }
+            }
+        }
+
+        if (line.find("PLAYER_2_FLOOR_LINE") != std::string::npos) {
+            getData(line, data);
+            
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
+            }
+        }
+
+        for (int i = 0; i <= 4; i++) {
+            std::string playerMosaic = "PLAYER_2_MOSAIC_";
+            playerMosaic += std::to_string(i);
+            if (line.find(playerMosaic) != std::string::npos) {
+                getData(line, data);
+
+                dl = data.length();
+                for (k = 0; k < dl; k++) {
+                    std::cout << data[k] << std::endl;
+                }
+            }
+        }
+
+        //OTHER +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        if (line.find("CURRENT_PLAYER") != std::string::npos) {
+            getData(line, data);
+
+            dl = data.length();
+            for (k = 0; k < dl; k++) {
+                std::cout << data[k] << std::endl;
             }
         }
     }
-
-    // what if bag is not at i = 0? factories could be at the front too
-    // bool found = false;
-
-    // if (!savedFile && (found == false))
-    // {
-    //     std::cout << "File was not found, please enter another file: " << std::endl;
-    //     std::string filename;
-    //     std::cin >> filename;
-
-    //     LoadSave *load = new LoadSave();
-    //     load->loadFile(filename);
-        
-    //     if (savedFile) {
-    //         found = true;
-    //     }
-    // }
-    
-    // while (!savedFile.eof())
-    // {
-    //     std::string line;
-    //     for (int i = 0; getline(savedFile, line) && i < 35; i++)
-    //     {
-    //         int j, k, tll;
-
-    //         std::string data;
-    //         
-            
-
-    //         // BAG
-    //         if (i == 0)
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-
-    //         // LID
-    //         if (i == 1)
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-
-    //         // FACTORIES ----------------------------
-    //         // factory centre
-    //         if (i == 2) 
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-
-    //         // factory 1-5
-    //         for (j = 3; j <= 7; j++)
-    //         {
-    //             if (i == j)
-    //             {
-    //                 getData(line, data);
-
-    //                 // split data
-    //                 tll = data.length();
-    //                 for (k = 0; k < tll; k++)
-    //                 {
-    //                     std::cout << data[k] << std::endl;
-    //                 }
-    //             }
-    //         }
-
-    //         // PLAYER_1 -------------------------------
-    //         // name
-    //         if (i == 8)
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-
-    //         // score
-    //         if (i == 9)
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-
-    //         // patternLine1-5
-    //         for (j = 10; j <= 14; j++)
-    //         {
-    //             if (i == j)
-    //             {
-    //                 getData(line, data);
-
-    //                 // split data
-    //                 tll = data.length();
-    //                 for (k = 0; k < tll; k++)
-    //                 {
-    //                     std::cout << data[k] << std::endl;
-    //                 }
-    //             }
-    //         }
-
-    //         // floorLine
-    //         if (i == 15)
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-
-    //         // Mosaic1-5
-    //         for (j = 16; j <= 20; j++)
-    //         {
-    //             if (i == j)
-    //             {
-    //                 getData(line, data);
-
-    //                 // split data
-    //                 tll = data.length();
-    //                 for (k = 0; k < tll; k++)
-    //                 {
-    //                     std::cout << data[k] << std::endl;
-    //                 }
-    //             }
-    //         }
-
-    //         // PLAYER_2 --------------------------------
-    //         // P2name
-    //         if (i == 21)
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-
-    //         // P2score
-    //         if (i == 22)
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-
-    //         // P2patternLine1-5
-    //         for (j = 23; j <= 27; j++)
-    //         {
-    //             if (i == j)
-    //             {
-    //                 getData(line, data);
-
-    //                 // split data
-    //                 tll = data.length();
-    //                 for (k = 0; k < tll; k++)
-    //                 {
-    //                     std::cout << data[k] << std::endl;
-    //                 }
-    //             }
-    //         }
-
-    //         // P2floorLine
-    //         if (i == 28)
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-
-    //         // P2Mosaic_0
-    //         for (j = 29; j <= 33; j++)
-    //         {
-    //             if (i == j)
-    //             {
-    //                 getData(line, data);
-
-    //                 // split data
-    //                 tll = data.length();
-    //                 for (k = 0; k < tll; k++)
-    //                 {
-    //                     std::cout << data[k] << std::endl;
-    //                 }
-    //             }
-    //         }
-
-    //         // NUMBER OF PLAYERS------------------------
-    //         // currentPlayers
-    //         if (i == 34)
-    //         {
-    //             getData(line, data);
-
-    //             // split data
-    //             tll = data.length();
-    //             for (k = 0; k < tll; k++)
-    //             {
-    //                 std::cout << data[k] << std::endl;
-    //             }
-    //         }
-    //     }
-    // }
-    // savedFile.close();
 }
 
-void LoadSave::getData(std::string line, std::string &data)
-{
+//Getting the data after the '='
+void getData(std::string line, std::string& data) {
     int ll;
-    char e = '=';
-    size_t found = line.find(e);
+    char e = '='; 
+    size_t found = line.find(e); 
     int el = 0;
-    if (found != std::string::npos)
-    {
+    if (found != std::string::npos) {
         el = found + 1;
     }
     ll = line.length();
 
-    data = line.substr(el, ll - el);
+    data = line.substr(el, ll-el);
 }
