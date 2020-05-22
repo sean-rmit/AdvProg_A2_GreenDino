f49d6525e792db47316928354fa2565f984825f9
Amy Nguyen s3783694 <s3783694@student.rmit.edu.au>
2020-05-22 14:46:18 +1000

    Cleaned up UI

--- gameFunc/LoadSaveFile.h
+++ gameFunc/LoadSaveFile.h
@@ -13,10 +13,13 @@ public:
     // copy constructor
     LoadSave(LoadSave& other);
 
+    // function to manage saving files
     void saveFile(std::string saveFile, Player *player1, Player *player2, Centre *centre, Factories *factories, Bag *bag, Lid *lid, int currentPlayer);
 
+    // function to manage loading files
     void loadFile(std::string loadFile, Player *player1, Player *player2, Centre *centre, Factories *factories, Bag *bag, Lid *lid, int &currentPlayer);
 
+    // gets data from files during loading
     void getData(std::string line, std::string &data);
 
 };
